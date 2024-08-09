ca190fd68b8f3629ac17bbd0b6f93de309ea1a2f
Loïk É. Mallat <lemallat@outlook.com>
2024-08-08 23:14:57 -0400

    fix: change CacheManager::get function returns

--- src/abstractions/ICacheManager.ts
+++ src/abstractions/ICacheManager.ts
@@ -4,7 +4,7 @@ export default interface ICacheManager {
   caches: ICache<unknown, unknown>[];
 
   add(cache: ICache<unknown, unknown>): void;
-  get<K, V>(key: K): V | undefined;
+  get<K, V>(key: string): ICache<K, V> | undefined;
   remove(cache: ICache<unknown, unknown>): void;
   clear(): void;
 }
